35fffcfd92e8f981e66ec21933ebcf3033a638d8
Molly Offer-Westort <mollyow@gmail.com>
2022-01-20 09:57:44 -0600

    22

--- in-class/class_22.R
+++ in-class/class_22.R
@@ -5,7 +5,7 @@
 #' ---
 
 # install.packages("ggplot2") # you only need to install a package once
-library(ggplot2) # libraries you use are loaded every session
+library(ggplot2) # packages you use need to be loaded every session
 #' ggplot2 cheat sheet: https://github.com/rstudio/cheatsheets/blob/main/data-visualization.pdf
 
 #' 
@@ -43,34 +43,40 @@ diff(range(dat0$wage, na.rm = TRUE))/.05
 
 ggplot(dat0, # data
        aes(x = wage)) + # aesthetic mapping
-  geom_histogram(bins = 30) # layer
+  geom_histogram(bins = 30, na.rm = TRUE) # layer
+
 
 #'
 #' ## Other options
 #' 
 ggplot(dat0, # data
        aes(x = wage, fill = 'blue')) + # aesthetic mapping
-  geom_histogram(bins = 30) # layer
+  geom_histogram(bins = 30, na.rm = TRUE) # layer
 
 ggplot(dat0, # data
        aes(x = wage)) + # aesthetic mapping
-  geom_histogram(bins = 30, fill = 'blue') # layer
+  geom_histogram(bins = 30, fill = 'blue', na.rm = TRUE) # layer
 
 ggplot(dat0, # data
        aes(x = wage, fill = as.factor(nj))) + # aesthetic mapping
-  geom_histogram(bins = 15, position = 'dodge') # layer
+  geom_histogram(bins = 15, position = 'dodge', na.rm = TRUE) # layer
 
 dat0$State <- factor(dat0$nj, labels = c('PA', 'NJ'))
 
 g <- ggplot(dat0, # data
             aes(x = wage, fill = State)) + # aesthetic mapping
-  geom_histogram(bins = 15, position = 'dodge', color = 'white') + # layer
-  theme_bw() + 
+  geom_histogram(bins = 15, position = 'dodge', color = 'white', na.rm = TRUE) + # layer
   xlab('Business wages') +
   ylab('Business counts') + 
   ggtitle('Wage by state, counts')
 
-g
+g + theme_bw()
+
+g + theme_dark()
+
+g + theme_void()
+
+# you can try library(ggthemes) if you want to look up more options
 
 #+ eval = FALSE
 ggplot_build(g) # don't evaluate this when compiling
@@ -79,7 +85,7 @@ ggplot_build(g) # don't evaluate this when compiling
 ggplot(dat0, aes(x = wage, fill = State)) +
   geom_histogram(aes(y=c(..count..[..group..==1]/sum(..count..[..group..==1]),
                          ..count..[..group..==2]/sum(..count..[..group..==2]))),
-                 position='dodge', bins = 15) + 
+                 position='dodge', bins = 15, na.rm = TRUE) + 
   scale_y_continuous(labels = scales::percent) +
   theme_bw() + 
   xlab('Business wages') +
@@ -94,24 +100,27 @@ g + facet_wrap(~State)
 
 g + facet_wrap(vars(State), scales = 'free_y')
 
+g + facet_wrap(vars(State), scales = 'free_y')+ 
+  theme(axis.text.x = element_text(angle = 45, vjust = -1, hjust = 0.25))
+
 
 #'
 #'  ##  Boxplots
 ggplot(dat0, # data
        aes(x = wage, y = State, fill = State)) +
-  geom_boxplot(notch = TRUE) + 
-  geom_jitter(height = 0.2)
+  geom_boxplot(notch = TRUE, na.rm = TRUE) + 
+  geom_jitter(height = 0.2, na.rm = TRUE)
 
 
 # Looking at this by state
-dat$State <- factor(dat0$nj, labels = c('PA', 'NJ'))
+dat$State <- factor(dat$nj, labels = c('PA', 'NJ'))
 
 ggplot(dat, # data
        aes(x = wage, y = as.factor(d+1), fill = State, color = State)) + 
   scale_fill_manual(values=c("pink", "wheat")) +
   scale_color_manual(values=c("grey", "mediumaquamarine")) +
-  geom_boxplot(outlier.shape = NA)+
-  geom_point(position=position_jitterdodge()) + 
+  geom_boxplot(outlier.shape = NA, na.rm = TRUE)+
+  geom_point(position=position_jitterdodge(), na.rm = TRUE) + 
   theme_bw() + 
   coord_cartesian(xlim = c(3, 6)) + 
   xlab('Wages') + 
@@ -127,8 +136,8 @@ ggplot(dat, # data
        aes(x = mgrs, y = as.factor(d+1), fill = State, color = State)) + 
   scale_fill_manual(values=c("pink", "wheat")) +
   scale_color_manual(values=c("grey", "mediumaquamarine")) +
-  geom_boxplot(outlier.shape = NA)+
-  geom_point(position=position_jitterdodge()) + 
+  geom_boxplot(outlier.shape = NA, na.rm = TRUE)+
+  geom_point(position=position_jitterdodge(), na.rm = TRUE) + 
   theme_bw() + 
   xlab('Number of managers') + 
   ylab('Wave') + 
@@ -138,8 +147,8 @@ ggplot(dat, # data
        aes(x = pt, y = as.factor(d+1), fill = State, color = State)) + 
   scale_fill_manual(values=c("pink", "wheat")) +
   scale_color_manual(values=c("grey", "mediumaquamarine")) +
-  geom_boxplot(outlier.shape = NA)+
-  geom_point(position=position_jitterdodge()) + 
+  geom_boxplot(outlier.shape = NA, na.rm = TRUE)+
+  geom_point(position=position_jitterdodge(), na.rm = TRUE) + 
   theme_bw() + 
   xlab('Number of part time employees') + 
   ylab('Wave') + 
@@ -149,8 +158,8 @@ ggplot(dat, # data
        aes(x = ft, y = as.factor(d+1), fill = State, color = State)) + 
   scale_fill_manual(values=c("pink", "wheat")) +
   scale_color_manual(values=c("grey", "mediumaquamarine")) +
-  geom_boxplot(outlier.shape = NA)+
-  geom_point(position=position_jitterdodge()) + 
+  geom_boxplot(outlier.shape = NA, na.rm = TRUE)+
+  geom_point(position=position_jitterdodge(), na.rm = TRUE) + 
   theme_bw() + 
   xlab('Number of full time employees') + 
   ylab('Wave') + 
@@ -162,7 +171,7 @@ ggplot(dat, # data
 #' 
 #' 
 #' Make a boxplot that shows number of full time equivalent employees by 
-#' wave and state. Adjust additional aesthetic values as you like. 
+#' wave and state. Look up `?geom_boxplot` and modify several different options. 
 #' 
 #' 
 #' 
